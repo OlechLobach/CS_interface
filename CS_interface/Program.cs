b67e8e64fad07f3c044c9c383a8ce4a54f9ab43a
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-03-17 19:37:58 +0200

    Add project files.

---
+++ CS_interface/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CS_interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
